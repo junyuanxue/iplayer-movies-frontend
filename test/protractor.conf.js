4ac4d9a94a7130f965ddc6f020a556a4ef01696f
Junyuan Xue <junyuanxue.is@gmail.com>
2016-06-04 12:59:24 +0100

    Installed and set up Protractor

---
+++ test/protractor.conf.js
@@ -0,0 +1,14 @@
+exports.config = {
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: ['e2e/*.js'],
+  baseUrl: 'http://localhost:3000',
+  onPrepare: function() {
+    require('protractor-http-mock').config = {
+        rootDirectory: __dirname, // default value: process.cwd()
+        protractorConfig: 'protractor.conf.js' // default value: 'protractor-conf.js'
+    };
+
+    var SpecReporter = require('jasmine-spec-reporter');
+    jasmine.getEnv().addReporter(new SpecReporter({displayStacktrace: 'all'}));
+  }
+}
