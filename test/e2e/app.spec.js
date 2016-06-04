8e99e372776ce8b038152e536925fb1f68cb913d
Junyuan Xue <junyuanxue.is@gmail.com>
2016-06-04 13:59:16 +0100

    Wrote a Protractor test for the display of movies

--- test/e2e/app.spec.js
+++ test/e2e/app.spec.js
@@ -1,6 +1,56 @@
-describe('app', function () {
-  it('has a home page title', function () {
+describe('iPlayerMovies', function () {
+  var mock = require('protractor-http-mock');
+
+  beforeEach(function () {
+    mock([{
+      request: {
+        path: 'http://localhost:8000/movies/',
+        method: 'GET',
+      },
+      response: {
+        data: [
+          {
+            'title': 'Sydney White',
+            'synopsis': 'Modern Snow White',
+            'duration': 6060,
+            'channel': 'BBC Two',
+            'rating': 5
+          },
+          {
+            'title': 'Emma',
+            'synopsis': 'Jane Austen classic',
+            'duration': 6840,
+            'channel': 'BBC Two',
+            'rating': 7
+          }
+        ]
+      }
+    }]);
+
     browser.get('/');
+  });
+
+  afterEach(function () {
+    mock.teardown();
+  });
+
+  it('has a home page title', function () {
     expect(browser.getTitle()).toEqual('What\'s on BBC iPlayer?');
+  });
+
+  it('fetchs and displays a list of movies', function () {
+    var titles = $$('.movies p:nth-child(1)');
+    var ratings = $$('.movies p:nth-child(2)');
+    var synopses = $$('.movies p:nth-child(3)');
+    var duration = $$('.movies p:nth-child(4)');
+    var channels = $$('.movies p:nth-child(5)');
+
+    expect(titles.first().getText()).toEqual('Sydney White');
+    expect(ratings.first().getText()).toEqual('5/10');
+    expect(ratings.last().getText()).toEqual('7/10');
+    expect(synopses.last().getText()).toEqual('Jane Austen Classic');
+    expect(duration.first().getText()).toEqual('101 mins');
+    expect(duration.last().getText()).toEqual('114 mins');
+    expect(channels.first().getText()).toEqual('On BBC Two');
   });
 });
