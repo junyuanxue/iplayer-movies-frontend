0a754e261d96df4a46230ebc4ffd8af291c6fa9e
Junyuan Xue <junyuanxue.is@gmail.com>
2016-06-04 19:46:45 +0100

    Created index routing to serve up index.html

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ var app = express();
 var port = process.env.PORT || 3000;
 var path = require('path');
 
-app.use(express.static(path.join(__dirname, '/public')));
+var index = require('./app/routes/index');
 
 app.use('/', index);
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.listen(port, function () {
   console.log('App listening on port ' + port);
